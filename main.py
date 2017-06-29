8e473f64347fd7b695b7fb2d15e3aaae47f22256
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-06-29 18:01:26 +0300

    changed javascript

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ from wtforms.validators import Required
 
 WIT_AI_KEY = "UZ6QX46MOWTVGUHG4ZLSBVG7ADIJAL7A"  # Wit.ai keys are 32-character uppercase alphanumeric strings
 
-
 UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
 ALLOWED_EXTENSIONS = set(['wav'])
-
 
 
 class SpeachForm(Form):
@@ -28,59 +26,9 @@ def hello():
 	if request.method == "POST":
 		if form.validate_on_submit():
 			flash(form.text.data)
-		else:
-			flash('form not valid!')
-	else:
-		flash('not post!')
-			#return render_template('index.html', form=form)
+
 	return render_template('index.html', form=form)
 
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
-
-
-
-
-'''
-@app.route("/save", methods=["POST", "GET"])
-def save():
-
-	if request.method == "POST":
-		if 'file' not in request.files:
-			flash('No file part')
-			print('No file part')
-			return redirect(request.url)
-
-		file = request.files['file']
-		if file.filename == '':
-			flash('No selected file')
-			print('No selected file')
-			return redirect(request.url)
-
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return '123123'
-
-	AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "english.wav")
-
-	r = sr.Recognizer()
-	with sr.AudioFile(AUDIO_FILE) as source:
-		audio = r.record(source)
-
-	try:
-		text = r.recognize_wit(audio, key=WIT_AI_KEY)
-		print("Wit.ai thinks you said " + text)
-		return text
-	except sr.UnknownValueError:
-		print("Wit.ai could not understand audio")
-		return "Wit.ai could not understand audio"
-	except sr.RequestError as e:
-		print("Could not request results from Wit.ai service; {0}".format(e))
-		return "Could not request results from Wit.ai service; {0}".format(e)
-
-	
-'''
-
